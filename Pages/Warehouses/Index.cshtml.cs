3d64af03856b88c831fc7d333c627851c3ec2769
Yevhenii Danoi <jeniadan1302@gmail.com>
2021-05-17 16:19:08 +0300

    Сортировка и пагинация Warehouse

--- Pages/Warehouses/Index.cshtml.cs
+++ Pages/Warehouses/Index.cshtml.cs
@@ -18,15 +18,49 @@ namespace BuildingMaterials.Pages.Warehouses
         {
             _context = context;
         }
+        public string NameSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public IList<Warehouse> Warehouse { get; set; }
+        public PaginatedList<Warehouse> Warehouse { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortMaterial, string currentFilter, string searchString, int? pageIndex)
         {
-            Warehouse = await _context.Warehouses
+            CurrentSort = sortMaterial;
+            NameSort = string.IsNullOrEmpty(sortMaterial) ? "name_desc" : "";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            CurrentFilter = searchString;
+
+            IQueryable<Warehouse> warehouseIQ = from w in _context.Warehouses
+                                                select w;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                warehouseIQ = warehouseIQ.Where(o => o.Order.Material.Name.Contains(searchString));
+            }
+
+            switch (sortMaterial)
+            {
+                case "name_desc":
+                    warehouseIQ = warehouseIQ.OrderByDescending(o => o.Order.Material.Name);
+                    break;
+                default:
+                    warehouseIQ = warehouseIQ.OrderBy(o => o.Order.Material.Name);
+                    break;
+            }
+
+            int pageSize = ConstantValues.pageSize;
+
+            Warehouse = await PaginatedList<Warehouse>.CreateAsync(warehouseIQ
                 .Include(w => w.Order)
-                .ThenInclude(w => w.Material)
-                .ToListAsync();
+                .ThenInclude(w => w.Material), pageIndex ?? 1, pageSize);
         }
     }
 }
